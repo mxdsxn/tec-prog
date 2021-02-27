497875c51ecb30af5938b28487578eebd86cb51b
Madson Santana <madson.santana@dextra-sw.com>
2021-02-27 02:39:19 -0300

    chore(box-test): set box test class

---
+++ box-test.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "./box.hpp"
+#include "./box-test.hpp"
+
+using namespace std;
+
+BoxTest::BoxTest()
+{
+ box1 = nullptr;
+ box2 = nullptr;
+}
+BoxTest::~BoxTest() {}
+
+void BoxTest::startTest()
+{
+ float width, height, depth;
+
+ cout << "1 - Enter width, height and depth:\n";
+
+ cin >> width;
+ cin >> height;
+ cin >> depth;
+
+ Box *newBox1 = new Box();
+ newBox1->setWidth(width);
+ newBox1->setHeight(height);
+ newBox1->setDepth(depth);
+
+ box1 = newBox1;
+
+ cout << "2 - Enter width, height and depth:\n";
+
+ cin >> width;
+ cin >> height;
+ cin >> depth;
+
+ Box *newBox2 = new Box();
+ newBox2->setWidth(width);
+ newBox2->setHeight(height);
+ newBox2->setDepth(depth);
+
+ box2 = newBox2;
+
+ cout << "\nArea:\n";
+ cout << "Box 1: " << box1->area();
+ cout << "Box 2: " << box2->area();
+ cout << "\n";
+
+ cout << "\nVolume:\n";
+ cout << "Box 1: " << box1->volume();
+ cout << "Box 2: " << box2->volume();
+ cout << "\n";
+}
---
+++ box-test.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "./box.hpp"
+
+class BoxTest
+{
+private:
+ Box *box1;
+ Box *box2;
+
+public:
+ BoxTest();
+ ~BoxTest();
+
+ void startTest();
+}
